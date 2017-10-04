b75db1cdf4858afc998aae98675dc1b7a5a64130
Marquess <marquessavaldez@gmail.com>
2017-10-03 17:55:02 -0700

    Finished insert method and unit tests for said method

--- red_black_tree.hpp
+++ red_black_tree.hpp
@@ -1,15 +1,293 @@
+#ifndef RED_BLACK_TREE_HPP
+#define RED_BLACK_TREE_HPP
+
 #include "node.hpp"
+#include <vector>
+
+#include <iostream>
+
 using namespace std;
 
-typename<class T>
+template <class T>
 class red_black_tree {
-  private:
-    node<T>* root;
-    unsigned int size;
   public:
-    red_back_tree();
+    node<T>* root;
+    unsigned int sze;
+    red_black_tree();
+    red_black_tree(T&);
+    red_black_tree(vector<T>&);
 
-    find(T target);
-    insert(T data);
-    remove(T data);
+    unsigned int size();
+
+    node<T>* parent(node<T>*);
+    node<T>* grandparent(node<T>*);
+    node<T>* uncle(node<T>*);
+
+    node<T>* find(T&);
+    void rotate_left(node<T>*);
+    void rotate_right(node<T>*);
+    node<T>* insert_repair_tree(node<T>*);
+    node<T>* insert(T&);
+    bool remove(T&);
+};
+
+template<class T>
+red_black_tree<T>::red_black_tree() : root(nullptr), sze(0) {}
+
+template<class T>
+red_black_tree<T>::red_black_tree(T& d)
+{
+  root = new node<T>*(d);
+  sze = 1; 
 }
+
+template<class T>
+red_black_tree<T>::red_black_tree(vector<T>& data)
+{
+  root = nullptr;
+  sze = 0;
+  for(T d : data) {
+    insert(d);
+  }
+}
+
+template<class T>
+unsigned int red_black_tree<T>::size()
+{
+  return sze;
+}
+
+template<class T>
+node<T>* red_black_tree<T>::parent(node<T>* child)
+{
+  if(child) {
+    return child->parent;
+  }
+  return nullptr;
+}
+
+template<class T>
+node<T>* red_black_tree<T>::grandparent(node<T>* child)
+{
+  if(child && child->parent) {
+    return child->parent->parent;
+  }
+  return nullptr;
+}
+
+template<class T>
+node<T>* red_black_tree<T>::uncle(node<T>* child)
+{
+  node<T>* gp = grandparent(child);
+  if(gp) {
+    if(gp->left == parent(child)) {
+      return gp->right;
+    }
+    return gp->left;
+  }
+  return nullptr;
+}
+
+template<class T>
+node<T>* red_black_tree<T>::find(T& target)
+{
+  node<T>* curr = root;
+  while(curr != nullptr && curr->data != target) {
+    if(target > curr->data) {
+      curr = curr->right;
+    }
+    else {
+      curr = curr->left;
+    }
+  }
+  return curr;
+}
+
+template<class T>
+void red_black_tree<T>::rotate_left(node<T>* n)
+{
+  node<T>* newnode = n->right;
+  n->right = newnode->left;
+  newnode->left = n;
+  newnode->parent = n->parent;
+  n->parent = newnode;
+}
+
+template<class T>
+void red_black_tree<T>::rotate_right(node<T>* n)
+{
+  node<T>* newnode = n->left;
+  n->left = newnode->right;
+  newnode->right = n;
+  newnode->parent = n->parent;
+  n->parent = newnode;
+}
+
+template<class T>
+node<T>* red_black_tree<T>::insert_repair_tree(node<T>* n)
+{
+  node<T>* p = parent(n);
+  //case1: n is root, it must be black.
+  if(!p) {
+    n->isRed = false;
+    root = n;
+    return n;
+  }
+  //case2: parent is black - nothing needs to be done.
+  if(p->isBlack()) {
+    return n;
+  }
+  node<T>* u = uncle(n);
+  node<T>* gp = n->parent->parent;
+  //case3: parent is red, uncle doesn't exist
+  if(p->isRed && (!u || u->isBlack())) {
+    if(p == gp->left) {
+      if(n == p->left) {
+        p->parent = gp->parent;
+        p->right = gp;
+        if(gp->parent) {
+          if(gp->parent->left == gp) {
+            gp->parent->left = p;
+          }
+          else {
+            gp->parent->right = p;
+          }
+        }
+        gp->parent = p;
+        gp->left = nullptr;    
+        p->isRed = false;
+        gp->isRed = true;
+        insert_repair_tree(p);
+      }
+      else {
+        n->left = p;
+        n->right = gp;
+        n->parent = gp->parent;
+        if(gp->parent) {
+          if(gp->parent->left == gp) {
+            gp->parent->left = p;
+          }
+          else {
+            gp->parent->right = p;
+          }
+        }
+        gp->parent = p->parent = n;
+        gp->left = p->right = nullptr;
+        gp->isRed = true;
+        n->isRed = false;
+        insert_repair_tree(n);
+      }
+    }
+    else {
+      if(n == p->left) {
+        n->left = gp;
+        p->parent = gp->parent;
+        if(gp->parent) {
+          if(gp->parent->left == gp) {
+            gp->parent->left = p;
+          }
+          else {
+            gp->parent->right = p;
+          }
+        }
+        gp->parent = n;
+        gp->right = nullptr;
+        p->isRed = false;
+        gp->isRed = true;
+        insert_repair_tree(gp);
+      }
+      else {
+        p->left = gp;
+        p->parent = gp->parent;
+        if(gp->parent) {
+          if(gp->parent->left == gp) {
+            gp->parent->left = p;
+          }
+          else {
+            gp->parent->right = p;
+          }
+        }
+        gp->parent = p;
+        gp->right = nullptr;
+        p->isRed = false;
+        gp->isRed = true;
+        insert_repair_tree(p);
+      }
+    }
+    return n;
+  }
+  //case4: parent is red, uncle is black
+  if(false) {
+    if(n == gp->left->right) {
+      rotate_left(p);
+      n = n->left;
+    }
+    else if(n == gp->right->left) {
+      rotate_right(p);
+      n = n->right;
+    }
+    if(n == p->left) {
+      rotate_right(gp);
+    }
+    else {
+      rotate_left(gp);
+    }
+    p->isRed = false;
+    gp->isRed = true;
+  } 
+  //case5: parent is red, uncle is red
+  else {
+    p->isRed = false;
+    u->isRed = false;
+    gp->isRed = true;
+    insert_repair_tree(gp); 
+    return n;
+  }
+}
+
+
+template<class T>
+node<T>* red_black_tree<T>::insert(T& data)
+{
+  if(!root) {
+    sze++;
+    return (root = new node<T>(data));
+  } 
+  node<T>* curr = root;
+  node<T>* pred = root;
+  bool wentRight;
+  while(curr) {
+    if(data == curr->data) {
+      return curr;
+    }
+    pred = curr;
+    if(data > curr->data) {
+      curr = curr->right;
+      wentRight = true;
+    }
+    else {
+      curr = curr->left;
+      wentRight = false;
+    }
+  }
+  curr = new node<T>(pred, data);
+  if(wentRight) {
+    pred->right = curr;
+  }
+  else {
+    pred->left = curr;
+  }
+  insert_repair_tree(curr);
+  sze++;
+  return curr;
+}
+
+template<class T>
+bool red_black_tree<T>::remove(T& target)
+{
+  //TODO
+  return true;
+}
+
+
+#endif
--- tests.cpp
+++ tests.cpp
@@ -1,11 +1,52 @@
 #include "node.hpp"
+#include "red_black_tree.hpp"
 #include <gtest/gtest.h>
 
 TEST(NodeToggleColorTest, RedBlack) {
   node<int> a(nullptr, nullptr, nullptr, false, 1);
-  ASSERT_EQ(true, a.toggleColor());
   ASSERT_EQ(false, a.toggleColor());
-  ASSERT_EQ(true, a.isBlack());
+  ASSERT_EQ(true, a.toggleColor());
+  ASSERT_EQ(false, a.isBlack());
+}
+
+TEST(InsertTest, InsertFind) {
+  vector<int> nums = {5, 4, 1, 3, 2, 6, 7, 8};
+  red_black_tree<int> tree(nums);
+  for(int n : nums) {
+    ASSERT_NE(nullptr, tree.find(n));
+  }
+  int x = 9999;
+  ASSERT_EQ(nullptr, tree.find(x));
+}
+
+TEST(InsertTest, InsertCorrectness) {
+  vector<int> nums = {5, 4, 1, 3, 2, 6, 7, 8};
+  red_black_tree<int> tree(nums);
+  ASSERT_EQ(nums.size(), tree.size());
+
+  ASSERT_EQ(tree.root->data, 4);
+  ASSERT_EQ(tree.root->isRed, false);
+
+  ASSERT_EQ(tree.root->left->data, 2);
+  ASSERT_EQ(tree.root->left->isRed, false);
+
+  ASSERT_EQ(tree.root->left->left->data, 1);
+  ASSERT_EQ(tree.root->left->left->isRed, true);
+
+  ASSERT_EQ(tree.root->left->right->data, 3);
+  ASSERT_EQ(tree.root->left->right->isRed, true);
+
+  ASSERT_EQ(tree.root->right->data, 6);
+  ASSERT_EQ(tree.root->right->isRed, true);
+
+  ASSERT_EQ(tree.root->right->left->data, 5);
+  ASSERT_EQ(tree.root->right->left->isRed, false);
+
+  ASSERT_EQ(tree.root->right->right->data, 7);
+  ASSERT_EQ(tree.root->right->right->isRed, false);
+
+  ASSERT_EQ(tree.root->right->right->right->data, 8);
+  ASSERT_EQ(tree.root->right->right->right->isRed, true);
 }
 
 int main(int argc, char **argv)
